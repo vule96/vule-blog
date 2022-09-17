a40a29eb5f998003d54be612c489086e1f4cf9fc
vule <vulehuy2905@gmail.com>
2022-09-17 22:31:06 +0700

    removed redundant code on DonateSection

--- src/components/Section/DonateSection.tsx
+++ src/components/Section/DonateSection.tsx
@@ -1,10 +1,7 @@
 import { FC } from 'src/types/fc';
-
 import { Heading } from '../Heading';
-
 import { Section } from './Section';
 
-// export const Contact: FC<{ image: RandomPageImage }> = ({ image }) => {
 export const Donate: FC = () => {
   return (
     <Section id={'donate'}>
